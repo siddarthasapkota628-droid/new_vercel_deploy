84a4badfc6e267400adffd636d30e6dc261641f9
Siddartha <siddarthasapkota628@gmail.com>
2025-09-07 18:38:31 +0545

    Fix: avoid overwriting handler

--- api/index.py
+++ api/index.py
@@ -1,4 +1,3 @@
-from myproject.wsgi import application  # Adjust if your project name is different
 
-def handler(environ, start_response):
-    return application(environ, start_response)
+from myproject.wsgi import application as handler
+
