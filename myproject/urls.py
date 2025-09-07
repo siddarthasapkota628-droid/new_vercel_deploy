3de5c65cc3b5e90b21f5a8fff9fe79d28fbf3a8b
Siddartha <siddarthasapkota628@gmail.com>
2025-09-07 19:37:14 +0545

    Test minimal WSGI app on Vercel

--- myproject/urls.py
+++ myproject/urls.py
@@ -15,13 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.http import HttpResponse
+from django.urls import path,include
 
-def home(request):
-    return HttpResponse("Hello from Django on Vercel!")
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", home),
+    path("",include('myapp.urls')),
 ]
